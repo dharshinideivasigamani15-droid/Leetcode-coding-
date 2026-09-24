adf80b03732fb7115a1f6473f6c47e8a181d3bad
Sivadharshini D <dharshinideivasigamani15@gmail.com>
2026-09-24 10:30:36 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,18 +3,18 @@ import java.util.Map;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Map to store value -> index
+    
         Map<Integer, Integer> map = new HashMap<>();
         
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             
-            // If complement exists in map, we found our pair
+            
             if (map.containsKey(complement)) {
                 return new int[] { map.get(complement), i };
             }
             
-            // Store current number and its index
+            
             map.put(nums[i], i);
         }
         
